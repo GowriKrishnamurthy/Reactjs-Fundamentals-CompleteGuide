12777ce6712cdb4517d2db7cffbaafea9dae3ac5
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-11-23 17:15:30 +1100

    React state and setstate

--- src/App.js
+++ src/App.js
@@ -4,16 +4,36 @@ import './App.css';
 import Person from './Person/Person'; 
 
 class App extends Component {
+  state ={
+    persons:[
+      { name:'Tom',age:28 },
+      { name:'Harry',age:30 },
+      { name:'Rose',age:25 }
+    ]
+  }
+
+  switchNameHandler=()=> {
+    //DONT DO THIS... this.state.persons[0].name='Thomas';
+    this.setState({
+      persons:[
+        { name:'Thomas',age:28 },
+        { name:'Harry',age:30 },
+        { name:'Roseline',age:25 }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hi, I'm a React App!</h1>
         
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         {/* Using own react component*/}
-        <Person name='Tom' age='28' /> 
-        <Person name='Harry' age='30'> My hobbies:Racing </Person>
-        <Person name='Rose' age='25'/> 
-      </div>      
+        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age} /> 
+        <Person name= {this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies:Racing </Person>
+        <Person name= {this.state.persons[2].name} age={this.state.persons[2].age} /> 
+      </div>
 
       // JSX is just syntactic sugar for JavaScript, allowing us to write HTMLish code instead of
       // nested React.createElement(...) calls.
