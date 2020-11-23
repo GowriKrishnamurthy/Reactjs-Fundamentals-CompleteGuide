e49d7d9e4ef403ee7e3291d536974eef92664dfe
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-11-23 16:32:58 +1100

    added a React functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'; 
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <h1> Hi, I'm a React App!</h1>
-      // </div>      
-      React.createElement(
-        'div',
-        {className:'App'},
-        React.createElement('h1',null,"Hi, I\'m a React App!"))
+      <div className="App">
+        <h1> Hi, I'm a React App!</h1>
+        
+        {/* Using own react component*/}
+        <Person /> 
+      </div>      
+
+      // JSX is just syntactic sugar for JavaScript, allowing us to write HTMLish code instead of
+      // nested React.createElement(...) calls.
+      // React.createElement(
+      //   'div',
+      //   {className:'App'},
+      //   React.createElement('h1',null,"Hi, I\'m a React App!"))
     );
   }
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// Functional component
+// A component is a function that returns/ renders some JSX code 
+// it defines which HTML code React should render to the real DOM in the end.
+
+const person = () => {
+    return <p> I'm a Person!</p>
+};
+
+export default person; 
