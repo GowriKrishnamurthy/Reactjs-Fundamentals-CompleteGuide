af5ea24dd2234c45f8359530116c427e6c73be11
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-11-26 21:34:52 +1100

    Folder restructuring

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,10 +8,10 @@ const person = (props) => {
             width: '450px'
             }
     };    
-    const rand = Math.random()
-    if(rand > 0.7){
-        throw new Error('Something went wrong!'); 
-    }
+    // const rand = Math.random()
+    // if(rand > 0.7){
+    //     throw new Error('Something went wrong!'); 
+    // }
 
     return(        
         <div className="Person" style={style}>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'; 
+import Person from './components/Persons/Person/Person'; 
 import Radium,{StyleRoot}  from 'radium';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
