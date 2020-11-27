ba6fa3c354812fe185a7f821c23b396b87cc387d
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-11-27 22:30:12 +1100

    cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './components/Persons/Person/Person'; 
+import Person from '../components/Persons/Person/Person'; 
 import Radium,{StyleRoot}  from 'radium';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
   state ={
