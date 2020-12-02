e1803c7c4603389bb68799f2a1b5468f82db4744
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-12-02 19:51:18 +1100

    useEffect: lifecycle hooks in functional components:

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import styles from './Cockpit.module.css'
 
 const cockpit = ( props ) => {
+
+    // executes for every rendered cycle of the cockpit
+    useEffect(()=>{
+        console.log('[Cockpit.js] useEffect');
+
+        // Run this code only when our persons changed        
+        setTimeout(()=>{
+            alert('Saved data to cloud!');
+            }, 1000);       
+    }, [props.persons]); 
+    // if empty array [] is passed to this 2nd param, code gets executed only when dependencies 
+    // If there are no dependencies, they can never change and therefore this can never rerun, it will run for the first time,
+    // that is the default but it will never run again.
+
+    // 2nd param of UseEffect can have multiple fields that this code may depend on.. eg [a,b,c]
+
+    //useEffect1()
+    //useEffect2()
+    
     const assignedClasses = [];
     let btnStyle='';
     if(props.showPersons){
