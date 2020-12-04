eed691bf136365293b36d9ee3d02060e2c75668f
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-12-04 20:03:10 +1100

    performation optimization using shouldComponentUpdate

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -17,7 +17,10 @@ class Persons extends Component
     shouldComponentUpdate(nextProps,nextState)
     {
         console.log('[Persons.js] shouldComponentUpdate');  
-        return true;
+        if(nextProps.persons !== this.props.persons){
+            return true; }
+        else {
+            return false; }
     }
 
     getSnapshotBeforeUpdate(prevProps,prevState)
